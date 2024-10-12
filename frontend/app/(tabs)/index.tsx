866e61a866424056e3642173af0a4fc8b4ef8c35
Carson Secrest <carsonsecrest@gmail.com>
2024-10-12 14:48:05 -0700

    create map search bar

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function HomeScreen() {
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} 
@@ -12,7 +15,17 @@ export default function HomeScreen() {
           latitudeDelta: 0.2,
           longitudeDelta: 0.02,
         }}
-      />
+        >
+          <View style={styles.overlay}>
+            <Ionicons name="search" size={24} color="black" />
+            <TextInput 
+              style={styles.input}
+              onChangeText={text => setText(text)}
+              value={text}
+              placeholder="Search"
+            />
+          </View>
+      </MapView>
     </View>
   );
 }
@@ -24,5 +37,26 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  overlay: {
+    position: 'absolute',
+    top: '10%',
+    width: '80%',
+    height: '5%',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+    borderRadius: 50,
+    borderColor: '#D9D9D9',
+    borderWidth: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    flexDirection: 'row',
+    padding: 10,
+  },
+  input: {
+    marginLeft: 5,
+  }
 });
