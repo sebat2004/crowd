50871582a900b55acee275a636d0171045938586
Carson Secrest <carsonsecrest@gmail.com>
2024-10-12 14:03:06 -0700

    create map view

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,8 +1,28 @@
-import {Text} from 'react-native'
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <Text>HOME</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} 
+        initialRegion={{
+          latitude: 47.655334,
+          longitude: -122.303520,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.02,
+        }}
+      />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
