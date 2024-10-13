6f0d40240ba7b99df2f1532ad5e1784c28f5eb88
Carson Secrest <carsonsecrest@gmail.com>
2024-10-13 01:45:53 -0700

    clean up profile tab

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,26 +1,50 @@
+<<<<<<< Updated upstream
 import { useState } from "react";
 import { View, Text } from "react-native";
+=======
+import React, { useState } from "react";
+import { View, Text, Modal, Pressable, TouchableWithoutFeedback } from "react-native";
+>>>>>>> Stashed changes
 import { LoginButton, LogoutButton } from "@/components/auth";
 import { useAuth0 } from "react-native-auth0";
 import { ProfilePicture } from "@/components/auth/ProfilePicture";
 
 export default function ProfileScreen() {
   const { user, error } = useAuth0();
+<<<<<<< Updated upstream
   const [isAttendingView, setIsAttendingView] = useState(true);
+=======
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!modalVisible);
+  };
+>>>>>>> Stashed changes
 
   return (
-    <View className="w-full h-[85%] flex items-center justify-between mt-24">
-      <View className="flex gap-10">
-        {user && (
-          <View className="w-[85%] flex-row justify-between items-center">
-            <View>
-              <Text className="text-2xl line-clamp-2">Welcome back,</Text>
-              <Text className="text-2xl font-bold line-clamp-2">
-                {user.name}!
-              </Text>
+    <View className="w-full h-[85%] flex items-center justify-between mt-24 p-3">
+      {user ? (
+        <>
+          <View className="flex gap-10">
+            {user && (
+              <View className="w-[85%] flex-row justify-between items-center">
+                <View>
+                  <Text className="text-2xl line-clamp-2">Welcome back,</Text>
+                  <Text className="text-2xl font-bold line-clamp-2">
+                    {user.name?.split(" ")[0]}!
+                  </Text>
+                </View>
+                <Pressable onPress={toggleModal}>
+                  <ProfilePicture />
+                </Pressable>
+              </View>
+            )}
+            <Text className="text-2xl font-semibold text-left">Past Events</Text>
+            <View className="flex items-start rounded-lg p-5 bg-gray-200 gap-y-1">
+              <View className="w-52 h-40 rounded-lg bg-white"></View>
             </View>
-            <ProfilePicture />
           </View>
+<<<<<<< Updated upstream
         )}
 
         <Text className="text-2xl font-semibold text-left">Past Events</Text>
@@ -28,11 +52,30 @@ export default function ProfileScreen() {
           <View className="w-52 h-40 rounded-lg bg-white"></View>
         </View>
       </View>
+=======
+>>>>>>> Stashed changes
 
-      {!user && <Text>Not logged in</Text>}
-      {error && <Text>{error.message}</Text>}
-
-      {user ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={toggleModal}
+          >
+            <TouchableWithoutFeedback onPress={toggleModal}>
+              <View className="flex-1 justify-start items-end">
+                <View className="mt-20 mr-4 bg-white rounded-lg shadow-md">
+                  <LogoutButton />
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+          </Modal>
+        </>
+      ):(
+        <>
+          <Text>Not logged in</Text>
+          <LoginButton></LoginButton>
+        </>
+      )}
     </View>
   );
 }
--- frontend/components/auth/Logout.tsx
+++ frontend/components/auth/Logout.tsx
@@ -13,8 +13,13 @@ export const LogoutButton = () => {
   };
 
   return (
+<<<<<<< Updated upstream
     <View className="w-40 bg-gray-200 rounded-md">
       <Button color="red" onPress={onPress} title="Log out" />
+=======
+    <View className="w-40 bg-blue-800 rounded-md">
+      <Button color="white" onPress={onPress} title="Log out" />
+>>>>>>> Stashed changes
     </View>
   );
 };
