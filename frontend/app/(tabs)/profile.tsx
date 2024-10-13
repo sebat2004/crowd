fe648087f8c390e60c9a714b1326ac24c3a0afd2
Carson Secrest <carsonsecrest@gmail.com>
2024-10-13 01:48:39 -0700

    fix merge conflicts

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,25 +1,16 @@
-<<<<<<< Updated upstream
 import { useState } from "react";
-import { View, Text } from "react-native";
-=======
-import React, { useState } from "react";
 import { View, Text, Modal, Pressable, TouchableWithoutFeedback } from "react-native";
->>>>>>> Stashed changes
 import { LoginButton, LogoutButton } from "@/components/auth";
 import { useAuth0 } from "react-native-auth0";
 import { ProfilePicture } from "@/components/auth/ProfilePicture";
 
 export default function ProfileScreen() {
   const { user, error } = useAuth0();
-<<<<<<< Updated upstream
-  const [isAttendingView, setIsAttendingView] = useState(true);
-=======
   const [modalVisible, setModalVisible] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!modalVisible);
   };
->>>>>>> Stashed changes
 
   return (
     <View className="w-full h-[85%] flex items-center justify-between mt-24 p-3">
@@ -44,16 +35,6 @@ export default function ProfileScreen() {
               <View className="w-52 h-40 rounded-lg bg-white"></View>
             </View>
           </View>
-<<<<<<< Updated upstream
-        )}
-
-        <Text className="text-2xl font-semibold text-left">Past Events</Text>
-        <View className="flex items-start rounded-lg p-5 bg-gray-200 gap-y-1">
-          <View className="w-52 h-40 rounded-lg bg-white"></View>
-        </View>
-      </View>
-=======
->>>>>>> Stashed changes
 
           <Modal
             animationType="fade"
