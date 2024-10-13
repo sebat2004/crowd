41b84821a6a656269ced2e104c0030432bdcbe76
Carson Secrest <carsonsecrest@gmail.com>
2024-10-13 08:17:23 -0700

    add search bar focus delay

--- frontend/components/search/EventList.tsx
+++ frontend/components/search/EventList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState, useMemo, useRef } from 'react';
 import { View, TextInput, Text, FlatList, ActivityIndicator, TouchableOpacity, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
@@ -10,6 +10,7 @@ export default function EventList({ setMapRegion, toggleModal }) {
   const [loading, setLoading] = useState(true);
   const [searchText, setSearchText] = useState('');
   const [error, setError] = useState(null);
+  const searchInputRef = useRef(null);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -26,6 +27,12 @@ export default function EventList({ setMapRegion, toggleModal }) {
       }
     }
     fetchEvents();
+
+    setTimeout(() => {
+      if (searchInputRef.current) {
+        searchInputRef.current.focus();
+      } 
+    }, 1000);
   }, []);
 
   const filteredEvents = useMemo(() => {
@@ -97,7 +104,8 @@ export default function EventList({ setMapRegion, toggleModal }) {
         <View className="relative w-10/12 flex-row h-[40px] justify-start items-center bg-white rounded-full border-[#D9D9D9] border-2 drop-shadow-md p-2.5">
           <Ionicons name="search" size={18} color="black" />
           <TextInput 
-            className="ml-1 text-gray-400"
+            ref={searchInputRef}
+            className="ml-1 text-gray-400 flex-1"
             onChangeText={setSearchText}
             value={searchText}
             placeholder="Search"
