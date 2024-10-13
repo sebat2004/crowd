44b4e2a223eec3ba526f0b2ff798512850182141
Seba <sebastiantorresola3@gmail.com>
2024-10-13 03:28:00 -0700

    Migrate event image uploads to one route

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import Stripe from "stripe";
-import sgMail from "@sendgrid/mail"
+import sgMail from "@sendgrid/mail";
 import QRCode from "qrcode";
 
 import { configuration } from "./config.js";
-
+import multer from "multer";
+const upload = multer();
 
 // Load environment variables from .env file
-import dotenv, { config } from "dotenv";
+import dotenv from "dotenv";
 import { S3, S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 
@@ -50,122 +51,82 @@ const eventSchema = new mongoose.Schema({
   attendees: Number,
   capacity: Number,
   admission_cost: Number,
+  imageURL: String,
   location: pointSchema,
 });
 
 const ticketSchema = new mongoose.Schema({
   ticketId: String,
   valid: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
-const Ticket = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model("Ticket", ticketSchema);
 
 eventSchema.index({ location: "2dsphere" });
 
 const Event = mongoose.model("Event", eventSchema);
 
-
 // Express app setup
-const endpointSecret = ""
+const endpointSecret = "";
 
-app.post("/webhook", express.raw({type: "application/json"}), async (req, res) => {
-  let event = req.body;
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    let event = req.body;
 
-  if (endpointSecret) {
-    const signature = req.headers['stripe-signature'];
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        signature,
-        endpointSecret
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return res.sendStatus(400);
-    }
-  }
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntent = event.data.object;
-      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-      await (new Ticket({ ticketId: paymentIntent.id })).save();
-      const qr = await QRCode.toBuffer(paymentIntent.id)
-      const msg = {
-        to: "TODO",
-        from: "noreply@pdx9280.com",
-        subject: "Your ticket has arrived",
-        text: "Thank you for your purchase. Your QR code ticket is attached.",
-        html: `<img src="cid:qrcode" alt="Ticket QR Code" />`,
-        attachments: [
-          {
-            content: qr.toString("base64"),
-            filename: "qrcode.png",
-            type: "image/png",
-            content_id: "qrcode"
-          }
-        ]
+    if (endpointSecret) {
+      const signature = req.headers["stripe-signature"];
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          endpointSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return res.sendStatus(400);
       }
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("email sent")
-        })
-        .catch(err => {
-          console.error(err)
-        })
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}.`);
+    }
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        console.log(
+          `PaymentIntent for ${paymentIntent.amount} was successful!`
+        );
+        await new Ticket({ ticketId: paymentIntent.id }).save();
+        const qr = await QRCode.toBuffer(paymentIntent.id);
+        const msg = {
+          to: "TODO",
+          from: "noreply@pdx9280.com",
+          subject: "Your ticket has arrived",
+          text: "Thank you for your purchase. Your QR code ticket is attached.",
+          html: `<img src="cid:qrcode" alt="Ticket QR Code" />`,
+          attachments: [
+            {
+              content: qr.toString("base64"),
+              filename: "qrcode.png",
+              type: "image/png",
+              content_id: "qrcode",
+            },
+          ],
+        };
+        sgMail
+          .send(msg)
+          .then(() => {
+            console.log("email sent");
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}.`);
+    }
+    res.send();
   }
-  res.send();
-});
-
-app.use(express.json());
-
-app.post("/upload", async (req, res) => {
-  const file = req.file;
-
-  try {
-    const parallelUploads3 = new Upload({
-      client: new S3({}) || new S3Client({}),
-      params: {
-        Bucket: configuration.Bucket,
-        Key: configuration.Key,
-        Body: file.buffer,
-      },
-
-      // optional tags
-      tags: [
-        /*...*/
-      ],
-
-      // additional optional fields show default values below:
-
-      // (optional) concurrency configuration
-      queueSize: 4,
-
-      // (optional) size of each part, in bytes, at least 5MB
-      partSize: 1024 * 1024 * 5,
-
-      // (optional) when true, do not automatically call AbortMultipartUpload when
-      // a multipart upload fails to complete. You should then manually handle
-      // the leftover parts.
-      leavePartsOnError: false,
-    });
-
-    parallelUploads3.on("httpUploadProgress", (progress) => {
-      console.log(progress);
-    });
-
-    await parallelUploads3.done();
-    res.send("File uploaded successfully");
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Error uploading file");
-  }
-});
-
+);
 
 // CRUD routes
 app.get("/events", async (req, res) => {
@@ -207,7 +168,32 @@ app.get("/events/:id", async (req, res) => {
   }
 });
 
-app.post("/events", async (req, res) => {
+app.post("/events", upload.any(), async (req, res) => {
+  const { headers, files } = req;
+  console.log(headers, files);
+  try {
+    const parallelUploads3 = new Upload({
+      client: new S3({}) || new S3Client({}),
+      params: {
+        Bucket: configuration.Bucket,
+        Key: configuration.Key,
+        Body: files[0].buffer,
+      },
+      queueSize: 4,
+      partSize: 1024 * 1024 * 25, // 25MB
+      leavePartsOnError: false,
+    });
+
+    parallelUploads3.on("httpUploadProgress", (progress) => {
+      console.log(progress);
+    });
+
+    await parallelUploads3.done();
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Error uploading file");
+  }
+  const imageURL = `https://crowd-image-bucket.s3.us-west-2.amazonaws.com/${configuration.Key}`;
   const body = {
     ...req.body,
     event_date: new Date(req.body.event_date),
@@ -215,11 +201,14 @@ app.post("/events", async (req, res) => {
       type: "Point",
       coordinates: req.body.coordinates,
     },
+    imageURL,
   };
-  console.log(body);
+
   const event = new Event(body);
+
   try {
     const savedEvent = await event.save();
+
     res.status(201).json(savedEvent);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -254,16 +243,16 @@ app.delete("/events/:id", async (req, res) => {
 
 app.post("/payment-sheet", async (req, res) => {
   try {
-    const customer = await stripe.customers.create()
-  
+    const customer = await stripe.customers.create();
+
     const ephemeralKey = await stripe.ephemeralKeys.create(
       { customer: customer.id },
       { apiVersion: "2024-09-30.acacia" }
     );
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: 100, 
-      currency: 'usd',
+      amount: 100,
+      currency: "usd",
       customer: customer.id,
       // In production, you should collect this from your customer
       automatic_payment_methods: {
@@ -275,23 +264,24 @@ app.post("/payment-sheet", async (req, res) => {
       paymentIntent: paymentIntent.client_secret,
       ephemeralKey: ephemeralKey.secret,
       customer: customer.id,
-      publishableKey: 'pk_test_51Q9GX8FDMnNxWG99BVAFvcTLzqEvl2UCpifsG8rWvQvORmuWb8UAZOY6t1qep9AWxs6XFYyzUOxuYkZsATYcLMr900WqWdpAwe'
+      publishableKey:
+        "pk_test_51Q9GX8FDMnNxWG99BVAFvcTLzqEvl2UCpifsG8rWvQvORmuWb8UAZOY6t1qep9AWxs6XFYyzUOxuYkZsATYcLMr900WqWdpAwe",
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
-})
+});
 
 app.post("/verify-ticket", async (req, res) => {
   try {
     const { ticketId } = req.body;
-    const ticket = await Ticket.findOne({ ticketId })
+    const ticket = await Ticket.findOne({ ticketId });
 
     if (!ticket) {
-      return res.join({ success: false, message: "No ticket" })
+      return res.join({ success: false, message: "No ticket" });
     }
-    
+
     if (!ticket.valid) {
       return res.json({ success: false, message: "Invalid ticket" });
     }
@@ -299,17 +289,16 @@ app.post("/verify-ticket", async (req, res) => {
     ticket.valid = false;
     await ticket.save();
 
-    res.json({ success: true, message: "Ticket verified and consumed" })
+    res.json({ success: true, message: "Ticket verified and consumed" });
   } catch (err) {
     res.status(500).json({ success: false, message: err.message });
   }
-})
+});
 
 // 404 route
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
-
 
 // Start the server
 const PORT = process.env.PORT || 3000;
